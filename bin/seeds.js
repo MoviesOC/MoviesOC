14197602d3739ea45e78222611c47b1522431148
Carla Naujoks <cnaujoks3@gmail.com>
2018-09-03 12:14:24 +0200

    add view movies add seeds, models

---
+++ bin/seeds.js
@@ -0,0 +1,19 @@
+const User = require('../models/User');
+const mongoose = require('mongoose');
+
+mongoose
+    .connect(
+        'mongodb://localhost/movies',
+        { useNewUrlParser: true }
+    )
+    .then(x => {
+        console.log(`Connected to Mongo! Database name: "${x.connections[0].name}"`);
+    })
+    .catch(err => {
+        console.error('Error connecting to mongo', err);
+    });
+
+User.create(users).then(usersFromDB => {
+    console.log(usersFromDB.length + 'were created');
+    mongoose.connection.close();
+});
