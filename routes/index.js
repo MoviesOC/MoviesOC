82b80399b783ba6c75372b81d0752ce32841eec0
Ojuna Faust <ojunafaust@MacbookHotspot.local>
2018-09-03 18:16:07 +0200

    put all route logic in index.js file, downsized view files to one movie-suggestion.hbs

--- routes/index.js
+++ routes/index.js
@@ -1,19 +1,20 @@
 const express = require('express');
-const axios = require('axios');
 const router = express.Router();
+const axios = require('axios');
 
 function randomNum(num) {
     return Math.ceil(Math.random() * num);
 }
-
-/* GET home page */
+// GET home page
 router.get('/', (req, res, next) => {
     res.render('index');
 });
 
-router.get('/movie-suggestion', (req, res, next) => {
+// GET Movie-suggestion page
+router.get('/movie-suggestion', ensureAuthenticated, (req, res, next) => {
     let { genre } = req.query;
     // if (!genre) genre = '35'; // Default value
+    // let comedyGenreId = '&with_genres=35';
 
     let baseUrl = 'https://api.themoviedb.org/3/discover/movie?api_key=';
     const apiKey = 'dc08a5c8305a1ec46c91df025d899797';
@@ -31,7 +32,7 @@ router.get('/movie-suggestion', (req, res, next) => {
             console.log('\n\n\n');
             console.log('-----------------------------------------');
             console.log(response.data.results[0].title);
-            res.render('movie-comedy', {
+            res.render('movie-suggestion', {
                 user: req.user,
                 data: response.data,
                 title: response.data.results[0].title
@@ -42,4 +43,12 @@ router.get('/movie-suggestion', (req, res, next) => {
         });
 });
 
+// Ensure only registered users have acces to movie-detail page:
+function ensureAuthenticated(req, res, next) {
+    if (req.isAuthenticated()) {
+        return next();
+    } else {
+        res.redirect('/auth/login');
+    }
+}
 module.exports = router;
