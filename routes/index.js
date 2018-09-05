1efc73c073d9109a3320437e76b152de6e01c8bd
Ojuna Faust <ojuna.faust@gmail.com>
2018-09-05 20:05:08 +0200

    delete hated.hbs and watched.hbs file and make one view  only

--- routes/index.js
+++ routes/index.js
@@ -6,20 +6,104 @@ const User = require('../models/User');
 const axios = require('axios');
 
 /*
-// --------> 1.) GET / home page
+// -------------------> 1.) GET / HOME PAGE -->> index.hbs
 */
 router.get('/', (req, res, next) => {
     res.render('index');
 });
 
 /*
-// --------> 2.) GET / to user profile page
+// -------------------> 2.) GET / MOVIE-SUGGESTION PAGE -->> movie-suggestion.hbs
 */
+
+router.get('/movie-suggestion', ensureAuthenticated, (req, res, next) => {
+    let { genre } = req.query;
+    let baseUrl = 'https://api.themoviedb.org/3/discover/movie?api_key=';
+    let baseImgUrl = 'https://image.tmdb.org/t/p/w342/';
+    const apiKey = process.env.MOVIEDB_API_KEY;
+    let language = '&language=en-US&with_original_language=en';
+    let voteAverage = '&vote_average.gte=6.5';
+    let page = '&page=' + randomNum(100);
+    let resultIndex = randomNum(20);
+    let movieGenreId = '';
+    if (genre) movieGenreId = '&with_genres=' + genre;
+
+    let movieUrl = ''.concat(baseUrl + apiKey + language + voteAverage + page + movieGenreId);
+    // (let noAdult = '&include_adult=false'; // include movies + 18 or not)
+
+    // Get API information with axios:
+    axios
+        .get(movieUrl)
+        .then(response => {
+            res.render('movie-suggestion', {
+                user: req.user,
+                data: response.data,
+                image: baseImgUrl + response.data.results[resultIndex].poster_path,
+                title: response.data.results[resultIndex].title,
+                releaseYear: response.data.results[resultIndex].release_date,
+                rating: response.data.results[resultIndex].vote_average,
+                plot: response.data.results[resultIndex].overview,
+                id: response.data.results[resultIndex].id,
+                genre: req.query.genre
+            });
+        })
+        .catch(err => {
+            console.error(err);
+        });
+});
+
+/*
+// --------> 2.1) Push like/hate/already seen movie to user profile page array
+*/
+
+router.post('/movie-suggestion/:category', (req, res, next) => {
+    const { title, tmdbId, picture } = req.body;
+    const ownerId = req.user._id;
+    const newMovie = new Movie({
+        title,
+        tmdbId,
+        picture,
+        category: req.params.category,
+        _owner: ownerId
+    });
+    newMovie
+        .save()
+        .then(movie => {
+            res.redirect('/movie-suggestion/?genre=' + req.body.genre);
+        })
+        .catch(error => {
+            console.log('ERROR', error);
+        });
+});
+
+/*
+// --------> 2.2) Get a random Number:
+*/
+function randomNum(num) {
+    return Math.ceil(Math.random() * num);
+}
+/*
+// --------> 2.3) Ensure only registered users have acces to movie-detail& user profile page:
+*/
+function ensureAuthenticated(req, res, next) {
+    if (req.isAuthenticated()) {
+        return next();
+    } else {
+        res.redirect('/auth/login');
+    }
+}
+
+/*
+// -------------------> 3.) GET / USER PROFILE PAGE -->> user-profile.hbs
+*/
+
 router.get('/user-profile', ensureAuthenticated, (req, res) => {
     res.render('user-profile', { user: req.user });
 });
 
-// 2.1) GET / user movie ---> liked page
+/*
+// -------------------> 4.) GET / USERS LIKED/HATED/WATCHED ARRAY PAGE -->> user-movies.hbs
+*/
 
 router.get('/user-movies/:category', (req, res, next) => {
     let movieCategory = req.params.category;
@@ -48,119 +132,39 @@ router.get('/user-movies/:category', (req, res, next) => {
     });
 });
 
-// // 2.2) GET / user movie ---> hated page
-
-// router.get('/user-hated', (req, res, next) => {
-//     Movie.find({ _owner: req.user._id }).then(movies => {
-//         let hated = [];
-//         movies.map(movie => {
-//             if (movie.category === 'hate') {
-//                 hated.push(movie);
-//             } else {
-//                 console.log('error');
-//             }
-//         });
-//         res.render('user-hated', { hated });
-//     });
-// });
-// // 2.3) GET / user movie ---> watched page
-
-// router.get('/user-watched', (req, res, next) => {
-//     Movie.find({ _owner: req.user._id }).then(movies => {
-//         let watched = [];
-//         movies.map(movie => {
-//             if (movie.category === 'watched') {
-//                 watched.push(movie);
-//             } else {
-//                 console.log('error');
-//             }
-//         });
-//         res.render('user-watched', { watched });
-//     });
-// });
-
 /*
-// --------> 3.) GET / to movie-suggestion page
+// --------> 4.1) Delete movie from list
 */
 
-router.get('/movie-suggestion', ensureAuthenticated, (req, res, next) => {
-    let { genre } = req.query;
-    let baseUrl = 'https://api.themoviedb.org/3/discover/movie?api_key=';
-    let baseImgUrl = 'https://image.tmdb.org/t/p/w342/';
-    const apiKey = process.env.MOVIEDB_API_KEY;
-    let language = '&language=en-US&with_original_language=en';
-    let voteAverage = '&vote_average.gte=6.5';
-    let page = '&page=' + randomNum(100);
-    let resultIndex = randomNum(20);
-    let movieGenreId = '';
-    if (genre) movieGenreId = '&with_genres=' + genre;
-
-    let movieUrl = ''.concat(baseUrl + apiKey + language + voteAverage + page + movieGenreId);
-    // let noAdult = '&include_adult=false';
-    // call API with axios:
-    axios
-        .get(movieUrl)
-        .then(response => {
-            // console.log('\n\n\n');
-            // console.log('-----------------------------------------');
-            // console.log(response.data.results);
-            res.render('movie-suggestion', {
-                user: req.user,
-                data: response.data,
-                image: baseImgUrl + response.data.results[resultIndex].poster_path,
-                title: response.data.results[resultIndex].title,
-                releaseYear: response.data.results[resultIndex].release_date,
-                rating: response.data.results[resultIndex].vote_average,
-                plot: response.data.results[resultIndex].overview,
-                id: response.data.results[resultIndex].id,
-                genre: req.query.genre
-            });
-        })
-        .catch(err => {
-            console.error(err);
-        });
-});
-
-// 3.1) Get a random Number:
-function randomNum(num) {
-    return Math.ceil(Math.random() * num);
-}
-
-// 3.2) Ensure only registered users have acces to movie-detail& user profile page:
-function ensureAuthenticated(req, res, next) {
-    if (req.isAuthenticated()) {
-        return next();
-    } else {
-        res.redirect('/auth/login');
-    }
-}
-
-/*
-// --------> 4.) Push like/hate/already seen movie to user profile page array
-*/
-
-router.post('/movie-suggestion/:category', (req, res, next) => {
-    const { title, tmdbId, picture } = req.body;
-    const ownerId = req.user._id;
-    const newMovie = new Movie({
-        title,
-        tmdbId,
-        picture,
-        category: req.params.category,
-        _owner: ownerId
-    });
-    newMovie
-        .save()
+router.get('/movie/:id/delete', (req, res, next) => {
+    console.log('going to delete');
+    Movie.findByIdAndRemove(req.params.id)
         .then(movie => {
-            res.redirect('/movie-suggestion/?genre=' + req.body.genre);
+            console.log('The movie was deleted!!!:' + movie);
+            res.redirect('/user-movies/' + movie.category);
         })
         .catch(error => {
-            console.log('ERROR', error);
+            console.log(error);
         });
 });
 
 /*
-// --------> 5.) Get Movie details
+// --------> 4.2) Edit movie from list
+*/
+
+router.get('/movies/:id/edit', (req, res, next) => {
+    let newCategory = req.query.category;
+    Movie.findById(req.params.id).then(movie => {
+        let oldCategory = movie.category;
+        movie.category = newCategory;
+        movie.save().then(updatedMovie => {
+            res.redirect('/user-movies/' + oldCategory);
+        });
+    });
+});
+
+/*
+// ------------------->  5.) GET / MOVIE DETAILS -->> details.hbs
 */
 
 router.get('/movies/:id', (req, res, next) => {
@@ -201,41 +205,11 @@ router.get('/movies/:id', (req, res, next) => {
                 console.error(err);
             });
     });
-    // console.log('MOVIE__________________________________', movie);
-});
-/*
-// --------> 6.) Delete Movie from DB+list
-*/
-
-router.get('/movie/:id/delete', (req, res, next) => {
-    console.log('going to delete');
-    Movie.findByIdAndRemove(req.params.id)
-        .then(movie => {
-            console.log('The movie was deleted!!!:' + movie);
-            res.redirect('/user-movies/' + movie.category);
-        })
-        .catch(error => {
-            console.log(error);
-        });
+    // console.log('MOVIE_____________', movie);
 });
 
 /*
-// --------> 7.) Change Category of movie
-*/
-
-router.get('/movies/:id/edit', (req, res, next) => {
-    let newCategory = req.query.category;
-    Movie.findById(req.params.id).then(movie => {
-        let oldCategory = movie.category;
-        movie.category = newCategory;
-        movie.save().then(updatedMovie => {
-            res.redirect('/user-movies/' + oldCategory);
-        });
-    });
-});
-
-/*
-// --------> 8.) Search movies
+// -------------------> 6.) Search movies
 */
 router.get('/find-movies', (req, res, next) => {
     let searchQuery = req.query.search;
