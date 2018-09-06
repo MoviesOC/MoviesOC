8922dad0c2c40283caebb4008fcc08fcab0e4615
Carla Naujoks <cnaujoks3@gmail.com>
2018-09-06 23:23:57 +0200

    edit user-profile view

--- routes/index.js
+++ routes/index.js
@@ -200,11 +200,14 @@ function ensureAuthenticated(req, res, next) {
 
 router.get('/user-profile', ensureAuthenticated, (req, res) => {
     Movie.find({ _owner: req.user }).then(movies => {
-        if ((movies.category = 'like')) {
-            let movies;
-        }
-        console.log('============================================', movies);
-        res.render('user-profile', { user: req.user, moviesNumber: movies.length });
+        console.log(movies[movies.length - 3]);
+        res.render('user-profile', {
+            user: req.user,
+            moviesNumber: movies.length,
+            lastLiked1: movies[movies.length - 1],
+            lastLiked2: movies[movies.length - 2],
+            lastLiked3: movies[movies.length - 3]
+        });
     });
 });
 
